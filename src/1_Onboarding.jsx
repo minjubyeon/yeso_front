9c73ba3f7b9ea2cf188d531e09865e9cfc971f9e
minjubyeon <minjubyeon444@gmail.com>
2025-08-10 14:20:57 +0900

    refacotor : circle-group 이름 변경

--- src/1_Onboarding.jsx
+++ src/1_Onboarding.jsx
@@ -6,27 +6,22 @@ export const Onboarding = () => {
     <div className="Onboarding">
       <div className="div">
         <div className="text-wrapper">마음에 물 주는 시간</div>
-
         <p className="p">
           하루 한 번,
           <br />
           나만의 식물과 감정을 나누세요.
         </p>
-
         //바
         <div className="step-bar">
           <div className="step-fill" />
           <div className="step-dot1" />
           <div className="step-dot2" />
         </div>
-
-        
-        <div className="overlap">
-          <div className="overlap-wrapper">
-            <div className="overlap-2">
-              <div className="circle-3" />
-
-              <div className="circle-4" />
+        <div className="circle-group">
+          <div className="circle-3">
+            <div className="circle-2">
+              <div className="circle-1" />
+              <div className="circle-0" />
             </div>
           </div>
 
@@ -37,13 +32,11 @@ export const Onboarding = () => {
             캐릭터
           </div>
         </div>
-
         <div className="group-wrapper">
           <div className="div-wrapper">
             <div className="text-wrapper-2">다음</div>
           </div>
         </div>
-
         <div className="text-wrapper-3">logo</div>
       </div>
     </div>
